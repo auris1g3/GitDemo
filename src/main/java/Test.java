650de7bb5d47403f5b1d52287466415c3da89286
Viacheslav Potapkin <potapkin.vi@gmail.com>
2020-03-26 00:37:11 +0200

    Added List<Integer> to Test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
         int a=0;
+        List<Integer> myList = new ArrayList<Integer>();
     }
 }
