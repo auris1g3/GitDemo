34bc6e3e2745b571fabb84d7c37f48e5760638fb
Viacheslav Potapkin <potapkin.vi@gmail.com>
2020-03-27 14:05:26 +0200

    Use off methods in NewTest

--- src/main/java/NewTest.java
+++ src/main/java/NewTest.java
@@ -4,17 +4,23 @@ import java.util.List;
 public class NewTest {
     public static void main(String[] args) {
         System.out.println("Hello");
-        int a=0;
+        int a = 0;
         List<Integer> myList = new ArrayList<Integer>();
         String str;
+        sayBye();
+        sayHello();
+        System.out.println(sum(5,3));
     }
-    public static void sayHello(){
+
+    public static void sayHello() {
         System.out.println("Hello");
     }
-    public static void sayBye(){
+
+    public static void sayBye() {
         System.out.println("Bye");
     }
-    public static Integer sum(int a, int b){
-        return a+b;
+
+    public static Integer sum(int a, int b) {
+        return a + b;
     }
 }
